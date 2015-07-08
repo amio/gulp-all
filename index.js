74a51f1a875622b1ea23f480901d06e06a87a917
Amio <amio.cn@gmail.com>
2015-07-08 18:08:07 +0800

    Raw code.

---
+++ index.js
@@ -0,0 +1,15 @@
+module.exports = function () {
+
+	var args = Array.isArray(arguments[0])
+		? arguments[0]
+		: [].slice.call(arguments)
+
+	var tasks = args.map(function (task) {
+		return new Promise(function (res, rej) {
+			task.on('finish', res)
+				.on('error', rej)
+		})
+	})
+
+	return Promise.all(tasks)
+}
