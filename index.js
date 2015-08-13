3f2b2a167d1267c886badfa7d9212165242a7515
Amio <amio.cn@gmail.com>
2015-08-13 23:59:49 +0800

    (refactor) code format

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 'use strict'
 
 module.exports = function () {
+  var args = Array.isArray(arguments[0])
+    ? arguments[0]
+    : [].slice.call(arguments)
 
-	var args = Array.isArray(arguments[0])
-		? arguments[0]
-		: [].slice.call(arguments)
+  var tasks = args.map(function (task) {
+    return new Promise(function (res, rej) {
+      task
+        .on('finish', res)
+        .on('error', rej)
+    })
+  })
 
-	var tasks = args.map(function (task) {
-		return new Promise(function (res, rej) {
-			task.on('finish', res)
-				.on('error', rej)
-		})
-	})
-
-	return Promise.all(tasks)
+  return Promise.all(tasks)
 }
